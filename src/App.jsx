ad3b62afa4922565f2bc7f8712eef58a59a790cf
omeryildirim0 <152308714+omeryildirim0@users.noreply.github.com>
2024-01-17 11:02:48 -0500

    fixed layout issue

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,20 @@
 import { Route, Routes } from "react-router-dom"
 import HomePage from "./pages/HomePage/HomePage"
 import SignIn from "./pages/AuthPages/SignIn"
+import Layout from "./layouts/Layout"
 
 
 function App() {
 
 
   return (
-    <>
+    
+    <Layout>
       <Routes>
         <Route path="/" element={ <HomePage />} />
         <Route path="/sign-in" element={ <SignIn />} />
       </Routes>
-    </>
+    </Layout>
   )
 }
 
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,10 +3,9 @@ import Layout from '../../layouts/Layout'
 
 const HomePage = () => {
   return (
-    <Layout>
+  
       <div>Welcome to the Home Page!</div>
-      {/* Rest of your home page content */}
-    </Layout>
+    
   )
 }
 
