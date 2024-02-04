0d8589c2f3fa4147f17c2a3231275f00c9404dc9
omeryildirim0 <152308714+omeryildirim0@users.noreply.github.com>
2024-02-04 13:20:19 -0500

    changes to the coach public profile page buttons added

--- src/pages/CoachPublicProfile/CoachPublicProfile.jsx
+++ src/pages/CoachPublicProfile/CoachPublicProfile.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom'; // if you are using react-router for routing
-import { Flex, Box, Text, Image, Heading, HStack, Tag, TagLabel, TagCloseButton } from '@chakra-ui/react';
+import { Flex, Box, Text, Image, Heading, HStack, Tag, TagLabel, Button, Divider, Spacer } from '@chakra-ui/react';
 import { doc, getDoc } from 'firebase/firestore';
 import { firestore } from '../../firebase/firebase'; // Adjust the path as necessary
 import useFetchAvailability from '../../hooks/useFetchAvailability';
@@ -11,6 +11,7 @@ const CoachPublicProfile = () => {
   const [availability, setAvailability] = useState([]);
   const { coachId } = useParams(); // get the coachId from the URL
   const { fetchedAvailabilities, isLoadingAvailabilities, fetchError } = useFetchAvailability(coachId);
+  const [selectedDuration, setSelectedDuration] = useState(null);
 
 
   useEffect(() => {
@@ -30,8 +31,14 @@ const CoachPublicProfile = () => {
     fetchCoach();
   }, [coachId]);
 
+  const handleDurationSelect = (minutes) => {
+    setSelectedDuration(minutes);
+    // ... additional logic to handle duration selection
+  };
+
+
   return (
-    <Flex direction={['column', 'row']} m="4">
+    <Flex direction={['column', 'row']} m="4" align="stretch">
       <Box flex="1" borderWidth="1px" borderRadius="lg" overflow="hidden" p="6">
         {coach && (
           <>
@@ -42,8 +49,30 @@ const CoachPublicProfile = () => {
           </>
         )}
       </Box>
-      <Box mt={4}>
-        <Text fontSize="xl" fontWeight="bold" mb={2}>Coach's Availabilities:</Text>
+
+      
+
+
+      <Box flex="2" mt={{ base: 4, md: 0 }} ml={{ base: 0, md: 4 }} pl={{ base: 0, md: 4 }}>
+        <Text fontSize="xl" fontWeight="bold" mb={2}>Book a Video Call</Text>
+        <HStack spacing={4}>
+          <Button
+            colorScheme="blue"
+            variant={selectedDuration === 30 ? 'solid' : 'outline'}
+            onClick={() => handleDurationSelect(30)}
+          >
+            30 Minutes
+          </Button>
+          <Button
+            colorScheme="blue"
+            variant={selectedDuration === 60 ? 'solid' : 'outline'}
+            onClick={() => handleDurationSelect(60)}
+          >
+            60 Minutes
+          </Button>
+        </HStack>
+        
+        
         {isLoadingAvailabilities ? (
           <Text>Loading...</Text>
         ) : fetchError ? (
