d08f08ee5246ecca83ab7f25669dec8ccfff7262
omeryildirim0 <152308714+omeryildirim0@users.noreply.github.com>
2024-03-02 22:25:50 -0500

    all the meeting info and the time zone is displayed

--- src/pages/Dashboard/UserDashboard.jsx
+++ src/pages/Dashboard/UserDashboard.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 import { collection, getDocs, query, where } from 'firebase/firestore';
 import { firestore } from '../../firebase/firebase';
 import { Box, Heading, List, ListItem, Link, Text, VStack, Divider } from '@chakra-ui/react';
-
+import moment from 'moment-timezone';
 
 const userDoc = JSON.parse(localStorage.getItem("user-info"));
 const userId = userDoc.uid;
@@ -13,17 +13,29 @@ const UserDashboard = () => {
 
   useEffect(() => {
     const fetchMeetings = async () => {
-      const q = query(collection(firestore, "users", userId, "meetings"))
+      const q = query(collection(firestore, "users", userId, "meetings"));
       const querySnapshot = await getDocs(q);
-      const meetingsData = querySnapshot.docs.map(doc => ({
-        id: doc.id,
-        ...doc.data()
-      }));
+      const meetingsData = querySnapshot.docs.map(doc => {
+        const data = doc.data();
+        return {
+          id: doc.id,
+          meetingID: data.meetingID,
+          start_time: data.start_time,
+          joinURL: data.joinURL
+          // Include other data fields as necessary
+        };
+      });
       setMeetings(meetingsData);
     };
 
     fetchMeetings();
   }, []);
+
+  const formatMeetingTime = (time) => {
+    const localTime = moment(time).tz(moment.tz.guess());
+    return `${localTime.format('YYYY-MM-DD HH:mm')} (${localTime.format('z')})`;
+  };
+
 
   return (
     <Box p={5}>
@@ -34,7 +46,7 @@ const UserDashboard = () => {
             <ListItem key={meeting.id} p={3} shadow="md" borderWidth="1px" borderRadius="lg">
               <VStack align="start">
                 <Text fontWeight="bold">Meeting ID: {meeting.meetingID}</Text>
-                <Text>Start Time: {meeting.start_time}</Text>
+                <Text>Start Time: {formatMeetingTime(meeting.start_time)}</Text>
                 <Link href={meeting.joinURL} color="teal.500" isExternal>
                   Join URL
                 </Link>
