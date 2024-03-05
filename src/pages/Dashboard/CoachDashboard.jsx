04c942a57d3768bda6be8941cddba2ca709f0ca3
omeryildirim0 <152308714+omeryildirim0@users.noreply.github.com>
2024-03-05 12:30:47 -0500

    scheduled meetings show on the coachdash

--- src/pages/Dashboard/CoachDashboard.jsx
+++ src/pages/Dashboard/CoachDashboard.jsx
@@ -1,14 +1,19 @@
 import { Box, Grid, VStack } from '@chakra-ui/react';
 import CoachProfile from './CoachProfile'
 import AvailabilityCalendar from './AvailabilityCalendar'
+import CoachMeetings from './CoachMeetings';
+
 
 const CoachDashboard = () => {
+  const userDoc = JSON.parse(localStorage.getItem("user-info"));
+  const coachId = userDoc.uid; 
+
   return (
     
     <VStack spacing={8} w="full">
       <CoachProfile />
       <AvailabilityCalendar />
-      
+      <CoachMeetings coachId={coachId}/>
     </VStack>
   )
 }
