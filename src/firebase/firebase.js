945079110a9ecdea395ad795e73d66819fce0184
omeryildirim0 <152308714+omeryildirim0@users.noreply.github.com>
2024-03-06 15:17:36 -0500

    replaced the firebase env variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -16,13 +16,13 @@ import { getFunctions } from "firebase/functions";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDhVSASvDdmYRCS-HbbNydjXPCJOvf_hvo",
-  authDomain: "vonsult.firebaseapp.com",
-  projectId: "vonsult",
-  storageBucket: "vonsult.appspot.com",
-  messagingSenderId: "913775115378",
-  appId: "1:913775115378:web:11a0085a446cc7300c96eb",
-  measurementId: "G-EPZ1DNCC5E"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
