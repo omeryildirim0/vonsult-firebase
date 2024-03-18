5a5903cf2949a190e14dffbeada27c25c6f7bbb6
omeryildirim0 <152308714+omeryildirim0@users.noreply.github.com>
2024-03-18 00:29:55 -0400

    after the meeting is created the slot is removed from the availabilities

--- src/stripe/Success.jsx
+++ src/stripe/Success.jsx
@@ -3,6 +3,7 @@ import { useLocation } from 'react-router-dom';
 import { doc, getDoc } from 'firebase/firestore';
 import { firestore } from '../firebase/firebase';
 import { getFunctions, httpsCallable } from 'firebase/functions';
+import useStoreAvailability from '../hooks/useStoreAvailability';
 
 const functions = getFunctions();
 
@@ -12,6 +13,7 @@ const Success = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState('');
   const userDoc = JSON.parse(localStorage.getItem("user-info"));
+  const { removeAvailability } = useStoreAvailability();
 
   const location = useLocation();
 
@@ -21,6 +23,43 @@ const Success = () => {
 
   const query = useQuery();
   const appointmentId = query.get('appointment_id');
+
+  const processStartTimeISO = (startTimeISO, timezone) => {
+    // Parse the ISO string to a Date object
+    const startDateTime = new Date(startTimeISO);
+  
+    // Round down to the nearest hour for the start time
+    startDateTime.setMinutes(0, 0, 0); // Reset minutes and seconds to zero
+  
+    // Format the date as 'YYYY-MM-DD'
+    const date = startDateTime.toISOString().split('T')[0];
+  
+    // Format the start time as 'h:mm A', removing the leading zero if any
+    const startTime = startDateTime.toLocaleTimeString('en-US', {
+      hour: 'numeric', // 'numeric' will not add a leading zero
+      minute: '2-digit',
+      hour12: true
+    });
+  
+    // Set the end time exactly one hour later
+    const endDateTime = new Date(startDateTime.getTime() + 60 * 60000); // Add 60 minutes to the start time
+    const endTime = endDateTime.toLocaleTimeString('en-US', {
+      hour: 'numeric', // 'numeric' will not add a leading zero
+      minute: '2-digit',
+      hour12: true
+    });
+  
+    // Combine start time, end time, and the provided timezone to match the Firestore format
+    const timeSlot = `${startTime} - ${endTime} (${timezone})`;
+  
+    console.log('Date:', date);
+    console.log('Time slot:', timeSlot);
+    return { date, timeSlot };
+  };
+  
+  
+  
+  
 
   const fetchAppointmentDetails = async (appointmentId) => {
     try {
@@ -46,6 +85,11 @@ const Success = () => {
       setMeetingLink(response.data.joinURL);
       console.log("Meeting created:", response.data);
       console.log("Meeting link:", response.data.joinURL);
+
+      const { date, timeSlot } = processStartTimeISO(appointmentDetails.startTimeISO, appointmentDetails.timezone);
+
+      // Call removeAvailability with the formatted date and slot.
+      await removeAvailability(appointmentDetails.coachId, date, timeSlot);
       
     } catch (err) {
       console.error("Error creating Zoom meeting:", err);
