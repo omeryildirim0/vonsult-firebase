473ac8c56288ea741fccbb34cba51d233d6de72c
omeryildirim0 <152308714+omeryildirim0@users.noreply.github.com>
2024-03-22 12:07:38 -0400

    contact us in the footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Box } from '@chakra-ui/react';
+import { Box, Link } from '@chakra-ui/react';
 
 const Footer = () => {
   return (
     <Box bg="blue.400" color="white" p={4} textAlign="center">
       © {new Date().getFullYear()} Vonsult. All rights reserved.
+      <Link href="mailto:support@vonsult.com" isExternal ml={2} style={{ textDecoration: 'underline' }}>
+        Contact Us
+      </Link>
     </Box>
   );
 };
