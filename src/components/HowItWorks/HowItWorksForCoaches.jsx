216b88f5819b3182c1203a8749b50e366f19149a
omeryildirim0 <152308714+omeryildirim0@users.noreply.github.com>
2024-03-26 14:42:27 -0400

    become a coach link in the homepage

--- src/components/HowItWorks/HowItWorksForCoaches.jsx
+++ src/components/HowItWorks/HowItWorksForCoaches.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
-import { Flex, Box, Heading, Text, Icon, VStack } from '@chakra-ui/react';
+import { Flex, Box, Heading, Text, Icon, VStack, LinkBox, LinkOverlay } from '@chakra-ui/react';
 import { EditIcon, TimeIcon, LinkIcon } from '@chakra-ui/icons';
+import { useNavigate } from 'react-router-dom';
 
 const HowItWorksForCoaches = () => {
+  const navigate = useNavigate();
+
   return (
     <VStack spacing={8} align="stretch" m={5}>
       <Heading textAlign="center">For Coaches</Heading>
       <Flex wrap="wrap" justifyContent="center" alignItems="center">
 
-        {/* Become a Coach */}
-        <Box p={5} boxShadow="md" borderRadius="lg" m={2} textAlign="center" width={['100%', '100%', '30%']}>
+        {/* Become a Coach - Now as a clickable box */}
+        <LinkBox as={Box} p={5} boxShadow="md" borderRadius="lg" m={2} textAlign="center" width={['100%', '100%', '30%']} onClick={() => navigate('/become-a-coach')}>
           <Icon as={EditIcon} w={10} h={10} color="gray.600" mb={3} />
           <Heading size="md" mb={2}>Become a Coach</Heading>
           <Text fontSize="md">
             Sign up and create your coach profile to showcase your expertise.
           </Text>
-        </Box>
+          <LinkOverlay href="/become-a-coach" />
+        </LinkBox>
 
         {/* Add Your Availability */}
         <Box p={5} boxShadow="md" borderRadius="lg" m={2} textAlign="center" width={['100%', '100%', '30%']}>
