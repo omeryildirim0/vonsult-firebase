b79ba1aebd86244d14169364cea787933f485b99
omeryildirim0 <152308714+omeryildirim0@users.noreply.github.com>
2024-04-29 21:03:09 -0400

    more progress on bennefits

--- src/pages/BecomeACoach/Benefits.jsx
+++ src/pages/BecomeACoach/Benefits.jsx
@@ -8,8 +8,12 @@ import {
     HStack,
     useBreakpointValue,
     Container,
+    AspectRatio,
     chakra
   } from '@chakra-ui/react';
+import benefit1 from '../../assets/benefit1.jpg';
+import benefit2 from '../../assets/benefit2.jpg';
+import benefit3 from '../../assets/benefit3.jpg';
   
   function Benefits() {
     // Determine the image's display side based on the index
@@ -19,17 +23,17 @@ import {
       {
         title: "Effortless Start",
         text: "Craft your personal brand effortlessly by uploading your picture and unique bio. We handle the technical side, so your focus remains on growing your influence.",
-        imgSrc: "path/to/start-earning-image.jpg", // Replace with actual image path
+        imgSrc: benefit1, // Replace with actual image path
       },
       {
         title: "Flexible Earnings",
         text: "Define your worth. With Vonsult, you're in control of your rates and schedule, ensuring that your talent is rightly valued.",
-        imgSrc: "path/to/set-price-image.jpg", // Replace with actual image path
+        imgSrc: benefit2, // Replace with actual image path
       },
       {
         title: "Simple Sharing",
         text: "Amplify your reach with your personal link. Embed it on your blog, bio, or email and watch your community thrive through direct engagements.",
-        imgSrc: "path/to/share-link-image.jpg", // Replace with actual image path
+        imgSrc: benefit3, // Replace with actual image path
       },
     ];
   
@@ -55,26 +59,23 @@ import {
   
   function BenefitItem({ title, text, imgSrc, isImageLeft }) {
     const direction = useBreakpointValue({ base: 'column', md: isImageLeft ? 'row' : 'row-reverse' });
-    
+    const imageBoxSize = useBreakpointValue({ base: '100%', md: '450px' });
+  
     return (
-      
-      
       <Flex
         direction={direction}
         justify="center"
         align="center"
         wrap="no-wrap"
       >
-        
-        <Box flex="1" mb={{ base: 4, md: 0 }}>
+        <AspectRatio ratio={4 / 3} w={imageBoxSize} mb={{ base: 4, md: 0 }}>
           <Image
             borderRadius="lg"
             src={imgSrc}
             alt={title}
-            maxH="300px"
             objectFit="cover"
           />
-        </Box>
+        </AspectRatio>
         <Box flex="1" px={{ base: 4, md: 8 }}>
           <Heading size="lg" mb={3}>{title}</Heading>
           <Text fontSize="md" mb={4}>{text}</Text>
@@ -82,6 +83,7 @@ import {
       </Flex>
     );
   }
+  
   
   export default Benefits;
   
