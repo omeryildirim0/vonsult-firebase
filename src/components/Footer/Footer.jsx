d446a98f53fdcb7a223551c5d5a2e43ef18b4d21
omeryildirim0 <152308714+omeryildirim0@users.noreply.github.com>
2024-05-02 10:54:40 -0400

    new footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
-import { Box, Link } from '@chakra-ui/react';
+import { Box, Container, Stack, Text, Link, useColorModeValue } from '@chakra-ui/react';
+import { FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <Box bg="blue.400" color="white" p={4} textAlign="center">
-      © {new Date().getFullYear()} Vonsult. All rights reserved.
-      <Link href="mailto:support@vonsult.com" isExternal ml={2} style={{ textDecoration: 'underline' }}>
-        Contact Us
-      </Link>
+    <Box
+      bg={useColorModeValue('gray.50', 'gray.900')}
+      color={useColorModeValue('gray.700', 'gray.200')}>
+      <Container as={Stack} maxW={'6xl'} py={4} spacing={4} justify={'center'} align={'center'}>
+        <Stack direction={'row'} spacing={6}>
+          <Link href={'#'}>Home</Link>
+          <Link href={'#'}>About</Link>
+          <Link href={'#'}>Blog</Link>
+          <Link href="mailto:support@vonsult.com" isExternal>Contact</Link>
+        </Stack>
+        <Text>© 2024 Vonsult. All rights reserved.</Text>
+        <Stack direction={'row'} spacing={6}>
+          <Link href={'https://www.instagram.com/vonsult_marketplace/'} isExternal>
+            <FaInstagram />
+          </Link>
+          
+        </Stack>
+      </Container>
     </Box>
   );
 };
