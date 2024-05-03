a81145caa2375d37d6592cca909d645d5aa7b0ed
omeryildirim0 <152308714+omeryildirim0@users.noreply.github.com>
2024-05-02 21:15:58 -0400

    link to home fixed and redeployed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
       color={useColorModeValue('gray.700', 'gray.200')}>
       <Container as={Stack} maxW={'6xl'} py={4} spacing={4} justify={'center'} align={'center'}>
         <Stack direction={'row'} spacing={6}>
-          <Link href={'#'}>Home</Link>
+          <Link href={'/'}>Home</Link>
           <Link href={'/about'}>About</Link>
           <Link href={'#'}>Blog</Link>
           <Link href="mailto:support@vonsult.com" isExternal>Contact</Link>
